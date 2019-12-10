58f6623df8d61b93c9b52283fcb1bcca37d0c434
gitmarcalinascris <47529366+gitmarcalinascris@users.noreply.github.com>
2019-12-10 22:51:57 +0800

    ui update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Jumbotron , Table} from 'reactstrap';
+import { Jumbotron, Table } from 'reactstrap';
 import "./Layout.css"
 import "./../index.css"
 import fbImg from "./../Assets/images/fb.png"
@@ -11,29 +11,29 @@ const Home = () => {
             <div id="HomeTitle">
                 <center>
                     <Jumbotron className="jumbotron-custom">
-                        <p className="lead" style={{marginTop:"-5px"}}><b>Welcome!</b></p>
+                        <p className="lead" style={{ marginTop: "-5px" }}><b>Welcome!</b></p>
                         <div className="div-title">What is Pasig Bible Believers Baptist Church and Missions (P3BCM)?</div>
                         <p id="p-desc" className="text-sm-center blockquote">
                             The P3BCM is dispensational KJV-only Baptist church in Metro Manila. It is an Independent, Old fashioned, Evangelistic and Missionary local Church. It believes, teaches and preaches the <b>King James Bible</b> also known as the <b>1611 Authorized Version </b>.The P3BCM <u><b>IS NOT</b></u> a “Baptist Brider”, a “Calvinist”, a “Hyper-dispensationalist”, a “Reformed Baptist”, an anti-“sinner’s prayer” nor an antisemitic church.
-                        </p>  
-                        <div className="bg-common" style={ {maxWidth: "400px" }}>
+                        </p>
+                        <br />
+                        <div className="bg-common" style={{ maxWidth: "400px" }}>
                             <p className="lead"><b>Pastor Ismael Sanchez.</b></p>
-                            <p className="lead">1542 E. Santos St., Barangay Palatiw.</p>
-                            <p className="lead">Pasig City, Philippines 1600</p>
-                            <br />
-                            <div className="lead">
+                            <p className="lead">1542 E. Santos St., Barangay Palatiw.
+                            Pasig City, Philippines 1600
+                            <div>
                                 ☎ 0999 352 4318
                                 <p>
                                     ✉ PasigKJV1611@gmail.com
                                 </p>
-                                <p style={{ marginTop: -10 }}>
-                                    <a href="https://www.facebook.com/Av1611BelieversBaptistChurchPasig/"><img src={fbImg} alt="logo" style={{ width: 32, height: 32 }} /> Visit our Facebook</a> 
-                                </p>
-                            </div>
-
+                                    <p style={{ marginTop: -10 }}>
+                                        <a href="https://www.facebook.com/Av1611BelieversBaptistChurchPasig/"><img src={fbImg} alt="logo" style={{ width: 32, height: 32 }} /> Visit our Facebook</a>
+                                    </p>
+                                </div>
+                            </p>
                         </div>
 
-    
+
                         <Table striped bordered size="sm" responsive className="tbl-display">
                             <thead>
                                 <tr>
